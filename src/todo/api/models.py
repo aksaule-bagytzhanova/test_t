8400012a76414902b821ce1ba3f282a98cd207c7
aksaule <aksaule124026@gmail.com>
2022-03-30 12:34:22 +0600

    Make models and other

--- src/todo/api/models.py
+++ src/todo/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    username = models.CharField(db_index=True, max_length=256, unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=256)
+    created_date = models.DateField(auto_now=True)
